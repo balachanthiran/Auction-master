ca9de8aea57c060a25e85b8e4d6a5045d28c8291
balachanthiran <varunsasy@hotmail.com>
2019-07-13 11:30:49 +0200

    Update Server.js

--- js/Server.js
+++ js/Server.js
@@ -9,7 +9,7 @@ var io = require('socket.io')(http);
 
 io.on('connection', function (socket) {
 	console.log("connection");
-	socket.on('nigger', function (from, msg) {
+	socket.on('test', function (from, msg) {
 		console.log('FRA:', from, 'INDHOLD:', msg);
 	});
 });
